41fcc8b9bc491d07bf7243e62b179d46f1c035a4
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-10-17 15:50:42 +0200

    tools: fix a couple of missing things, now compression works again

--- tools/utils/utils.h
+++ tools/utils/utils.h
@@ -21,6 +21,55 @@
 
 #include "list.h"
 
+// detect architecture
+#if defined(__x86_64__) || defined(_M_X64)
+#define __ARCHITECTURE_STR "amd64"
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+#define __ARCHITECTURE_STR "i386"
+#elif defined(__ARM_ARCH_2__)
+#define __ARCHITECTURE_STR "arm2"
+#elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
+#define __ARCHITECTURE_STR "arm3"
+#elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
+#define __ARCHITECTURE_STR "arm4t"
+#elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
+#define __ARCHITECTURE_STR "arm5"
+#elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
+#define __ARCHITECTURE_STR "arm6t2"
+#elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
+#define __ARCHITECTURE_STR "arm6"
+#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+#define __ARCHITECTURE_STR "arm7"
+#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+#define __ARCHITECTURE_STR "arm7a"
+#elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+#define __ARCHITECTURE_STR "arm7r"
+#elif defined(__ARM_ARCH_7M__)
+#define __ARCHITECTURE_STR "arm7m"
+#elif defined(__ARM_ARCH_7S__)
+#define __ARCHITECTURE_STR "arm7s"
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define __ARCHITECTURE_STR "arm64"
+#elif defined(mips) || defined(__mips__) || defined(__mips)
+#define __ARCHITECTURE_STR "mips"
+#elif defined(__sh__)
+#define __ARCHITECTURE_STR "superh"
+#elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+#define __ARCHITECTURE_STR "powerpc"
+#elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
+#define __ARCHITECTURE_STR "powerpc64"
+#elif defined(__sparc__) || defined(__sparc)
+#define __ARCHITECTURE_STR "sparc"
+#elif defined(__m68k__)
+#define __ARCHITECTURE_STR "m68k"
+#elif defined(__riscv32)
+#define __ARCHITECTURE_STR "riscv32"
+#elif defined(__riscv64)
+#define __ARCHITECTURE_STR "riscv64"
+#else
+#define __ARCHITECTURE_STR "unknown"
+#endif
+
 // include dirent.h for directory operations
 #if defined(_WIN32) || defined(_WIN64)
 #define __PATH_SEPARATOR '\\'
